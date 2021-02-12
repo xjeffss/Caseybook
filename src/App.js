9dbd4d4c5ccff60a841d0948405724b720f00eb1
xjeffss <elmorejeffreyp@johndeere.com>
2021-02-12 10:55:38 -0600

    Profile page initial friend list

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
       user:{
         name: "Casey Harding",
         username: "CRHarding",
-        image: "...",
-        friendList: [],
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Chris_Hemsworth_as_Thor.jpg/220px-Chris_Hemsworth_as_Thor.jpg",
+        friendList: ["Julie"],
       },
       apiDataLoaded: false,
       potentialFriends: []
@@ -22,7 +22,7 @@ class App extends Component {
       return (
     <div className="App">
       <h1>Caseybook</h1>
-      <Profile />
+      <Profile user={this.state.user} />
     </div>
      );
     }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Profile = (props) => {
     return(
-        <p>Profile</p>
+        <div>
+            <h3>{props.user.name}</h3>
+            <h3>{props.user.username}</h3>
+            <img src={props.user.image} />
+            {props.user.friendList.map(friend => <p>{friend}</p>)}
+
+        </div>
     )
 }
 
