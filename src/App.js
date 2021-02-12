4c13742662f17a5940a430e53d1c730d4ee056df
xjeffss <elmorejeffreyp@johndeere.com>
2021-02-12 09:46:43 -0600

    Initial Boilerplate info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user:{
+        name: "Casey Harding",
+        username: "CRHarding",
+        image: "...",
+        friendList: [],
+      },
+      apiDataLoaded: false,
+      potentialFriends: []
+    }
+  }
+  render() {
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Caseybook</h1>
     </div>
-  );
-}
+     );
+    }
+  }
+
 
 export default App;
